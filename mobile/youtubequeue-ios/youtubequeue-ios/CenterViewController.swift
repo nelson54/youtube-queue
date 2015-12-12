16368e6bf08dc2a834a2a3b716c8b876fe61d1ab
Michael <michael@michaelsimard.com>
2015-12-11 20:16:27 -0500

    generic parent for the center view, will handle events from the drawer approrpriately

--- mobile/youtubequeue-ios/youtubequeue-ios/CenterViewController.swift
+++ mobile/youtubequeue-ios/youtubequeue-ios/CenterViewController.swift
@@ -6,8 +6,57 @@
 //
 //
 
-import Cocoa
+import UIKit
+
+
+@objc
+protocol CenterViewControllerDelegate {
+    optional func toggleLeftDrawer()
+    //  optional func closeLeftDrawer()
+}
 
 class CenterViewController: UIViewController {
+    var delegate: CenterViewControllerDelegate?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let menuButtonItem = UIBarButtonItem(title:"Menu", style: UIBarButtonItemStyle.Plain, target: self, action: "menuTapped:")
+        let castButtonItem = UIBarButtonItem(title:"Cast", style: UIBarButtonItemStyle.Plain, target: self, action: "")
 
+        navigationItem.leftBarButtonItem = menuButtonItem
+        navigationItem.rightBarButtonItem = castButtonItem
+        navigationItem.rightBarButtonItem?.enabled = false
+        navigationItem.title = "Youtube Queue"
+    }
+    
+     func menuTapped(sender: AnyObject) {
+        delegate?.toggleLeftDrawer?()
+    }
 }
+
+extension CenterViewController:LeftDrawerViewControllerDelegate {
+    
+    func tappedQuickAdd(){
+      //  self.tappedAddButton()
+    }
+    
+    func quickConnectToDerek() {
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        let queueViewController = mainStoryBoard.instantiateViewControllerWithIdentifier("QueueViewController") as? QueueViewController
+        queueViewController!.roomId = "0iKjUBImIL"
+        
+        self.navigationController!.pushViewController(queueViewController!, animated: false)
+        queueViewController?.delegate = navigationController?.parentViewController as! ContainerViewController!
+        delegate?.toggleLeftDrawer?()
+        navigationItem.rightBarButtonItem?.enabled = true
+    }
+    
+    func tappedLeaveRoom() {
+        self.navigationController!.popToRootViewControllerAnimated(false)
+        delegate?.toggleLeftDrawer?()
+        navigationItem.rightBarButtonItem?.enabled = false
+
+    }
+}
+
+
+
