42b16248eb066e7b9d9259a687ca88dbdda863bf
Michael <michael@michaelsimard.com>
2015-12-11 20:09:47 -0500

    initial commits for drawers, containerviews

---
+++ mobile/youtubequeue-ios/youtubequeue-ios/CenterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.swift
+//  
+//
+//  Created by Michael on 12/11/15.
+//
+//
+
+import Cocoa
+
+class CenterViewController: UIViewController {
+
+}
---
+++ mobile/youtubequeue-ios/youtubequeue-ios/ContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContainerViewController.swift
+//  youtubequeue-ios
+//
+//  Created by Michael on 12/9/15.
+//  Copyright © 2015 michaelsimard. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
