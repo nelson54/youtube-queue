f3016327e6bfb570005574ac4a953d0702b39413
Michael <michael@michaelsimard.com>
2015-12-11 20:14:44 -0500

    left drawer, can join dereks room, leave a room. working..more coming

--- mobile/youtubequeue-ios/youtubequeue-ios/LeftDrawerViewController.swift
+++ mobile/youtubequeue-ios/youtubequeue-ios/LeftDrawerViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
+@objc
+protocol LeftDrawerViewControllerDelegate {
+    optional func tappedQuickAdd()
+     optional func quickConnectToDerek()
+    optional func tappedLeaveRoom()
+
+    //  optional func closeLeftDrawer()
+}
+
+
 class LeftDrawerViewController: UIViewController {
 
+    var delegate: LeftDrawerViewControllerDelegate?
+    let settingOptions:Array<String> = ["Quick Add", "Browse Youtube","Show Queue","Leave Room","Invite Friends","Rate Us","Derek"]
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    required init(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)!
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView .registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+        
 
         // Do any additional setup after loading the view.
     }
@@ -32,4 +57,65 @@ class LeftDrawerViewController: UIViewController {
     }
     */
 
+}
+
+
+// MARK: UITableViewDelegate
+extension LeftDrawerViewController :UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        switch(indexPath.row){
+        case 0:
+            print (0)
+            delegate?.tappedQuickAdd?()
+        case 1:
+            print(1)
+        case 2:
+            print(2)
+        case 3:
+            delegate?.tappedLeaveRoom?()
+            print(3)
+        case 4:
+            print(4)
+        case 5:
+            print (5)
+        case 6:
+            print (6)
+            delegate?.quickConnectToDerek?()
+        default:
+            print ("Default")
+        }
+    }
+    
+    //    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    //        return 100
+    //    }
+    
+}
+
+// MARK: UITableViewDatasource
+
+extension LeftDrawerViewController :UITableViewDataSource{
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")!
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                () -> Void in
+                cell.textLabel?.text = self.settingOptions[indexPath.row]
+                cell.textLabel?.textColor = UIColor.whiteColor()
+                cell.contentView.backgroundColor = UIColor.darkGrayColor()
+                cell.textLabel?.backgroundColor=UIColor.darkGrayColor()
+                cell.setNeedsLayout()
+            }
+    
+            return cell
+    }
+    
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            
+            return settingOptions.count
+    }
 }
