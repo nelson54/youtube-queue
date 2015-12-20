ed13c0848d4434b898e8ce87e6d89817a669e625
Michael <michael@michaelsimard.com>
2015-12-20 11:36:45 -0500

    fixed a random crash when trying to create and join a room

--- mobile/youtubequeue-ios/youtubequeue-ios/CenterViewController.swift
+++ mobile/youtubequeue-ios/youtubequeue-ios/CenterViewController.swift
@@ -15,55 +15,63 @@ protocol CenterViewControllerDelegate {
     //  optional func closeLeftDrawer()
 }
 
+
 class CenterViewController: UIViewController {
+
+    var networkManager: NSNetworkManager = NSNetworkManager()
+
     var delegate: CenterViewControllerDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
-        let menuButtonItem = UIBarButtonItem(title:"Menu", style: UIBarButtonItemStyle.Plain, target: self, action: "menuTapped:")
-        let castButtonItem = UIBarButtonItem(title:"Cast", style: UIBarButtonItemStyle.Plain, target: self, action: "castTapped:")
+        let menuButtonItem = UIBarButtonItem(title: "Menu", style: UIBarButtonItemStyle.Plain, target: self, action: "menuTapped:")
+        let castButtonItem = UIBarButtonItem(title: "Cast", style: UIBarButtonItemStyle.Plain, target: self, action: "castTapped:")
 
         navigationItem.leftBarButtonItem = menuButtonItem
         navigationItem.rightBarButtonItem = castButtonItem
         navigationItem.rightBarButtonItem?.enabled = false
         navigationItem.title = "Youtube Queue"
     }
-    
-     func castTapped(sender: AnyObject) {
-        
+
+    func castTapped(sender: AnyObject) {
+
     }
-    
-     func menuTapped(sender: AnyObject) {
+
+    func menuTapped(sender: AnyObject) {
         delegate?.toggleLeftDrawer?()
     }
-    
-    func joinRoom(roomId:String) {
 
-        let mainStoryBoard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
-        let queueViewController = mainStoryBoard.instantiateViewControllerWithIdentifier("QueueViewController") as? QueueViewController
-        queueViewController!.roomId = roomId
-        
-        self.navigationController!.pushViewController(queueViewController!, animated: false)
-        queueViewController?.delegate = navigationController?.parentViewController as! ContainerViewController!
+    func joinRoom(roomId: String) {
+
+        dispatch_async(dispatch_get_main_queue()) {
+            () -> Void in
+
+            let mainStoryBoard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+            let queueViewController = mainStoryBoard.instantiateViewControllerWithIdentifier("QueueViewController") as? QueueViewController
+            queueViewController!.roomId = roomId
+            self.navigationController!.pushViewController(queueViewController!, animated: false)
+            queueViewController?.delegate = self.navigationController?.parentViewController as! ContainerViewController!
+
+        }
     }
 }
 
 
+extension CenterViewController: LeftDrawerViewControllerDelegate {
 
-extension CenterViewController:LeftDrawerViewControllerDelegate {
-    
-    func tappedQuickAdd(){
-      //  self.tappedAddButton()
+    func tappedQuickAdd() {
+        //  self.tappedAddButton()
     }
-    
+
     func quickConnectToDerek() {
         joinRoom("0iKjUBImIL")
         delegate?.toggleLeftDrawer?()
     }
-    
+
     func tappedLeaveRoom() {
         self.navigationController!.popToRootViewControllerAnimated(false)
         delegate?.toggleLeftDrawer?()
-        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+        dispatch_async(dispatch_get_main_queue()) {
+            () -> Void in
             self.navigationItem.rightBarButtonItem?.enabled = false
         }
     }
